a667b50d74a10d07bec8a9acc64aeb2daa12f86c
pandaandco <pandaandcocardiff@gmail.com>
2016-04-13 17:14:30 +0100

    attempted again

--- personal/urls.py
+++ personal/urls.py
@@ -19,6 +19,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^$', views.personalityquiz, name='personalityquiz'),
-    url(r'^$', views.conservation, name='conservation'),    
+    url(r'^personalityquiz/', views.personalityquiz, name='personalityquiz'),
+    url(r'^conservation/', views.conservation, name='conservation'),    
 ]
