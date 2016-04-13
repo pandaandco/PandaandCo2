3f2f7f7a3a49b614286f643458631b13c97ec648
pandaandco <pandaandcocardiff@gmail.com>
2016-04-13 14:07:29 +0100

    links should work

--- personal/views.py
+++ personal/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'personal/home.html')
+    return render(request, 'personal/home.html','personal/conservation.html','personal/personalityquiz.html')
