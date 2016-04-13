c275efd400f87f06db42bb445b23dbd90e293626
pandaandco <pandaandcocardiff@gmail.com>
2016-04-13 14:59:53 +0100

    undo

--- personal/views.py
+++ personal/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, '../personal/home.html')
+    return render(request, 'personal/home.html')
 
 def conservation(request):
-    return render(request,'../personal/conservation.html')
+    return render(request,'personal/conservation.html')
 
 def personalityquiz(request):
-    return render(request,'../personal/personalityquiz.html')
+    return render(request,'personal/personalityquiz.html')
